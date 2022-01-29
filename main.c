08b5225919092baf374abad90d94e401182524e6
danielku <kucharski22daniel@gmail.com>
2022-01-29 20:38:35 +0100

    cmake config

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "SDL.h"
+
+int main(int argc, char **argv)
+{
+
+    int a;
+/*     SDL_Window test; */
+    SDL_SetWindowDisplayMode(SDL_Window *window, const SDL_DisplayMode *mode);
+    SDL_DisplayMode
+
+    a++;
+    return a;
+}
