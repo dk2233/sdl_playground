0cb431d4167e145f3017f92413d2161b9f03812c
danielku <kucharski22daniel@gmail.com>
2022-01-30 02:55:39 +0100

    open window and wait for event

--- main.c
+++ main.c
@@ -1,13 +1,69 @@
 #include "SDL.h"
+#include "SDL_events.h"
+#include "SDL_render.h"
+#include "SDL_surface.h"
+#include "SDL_video.h"
+
+#define WINDOW_TITLE (const char*)"title789"
 
 int main(int argc, char **argv)
 {
 
     int a;
-/*     SDL_Window test; */
-    SDL_SetWindowDisplayMode(SDL_Window *window, const SDL_DisplayMode *mode);
-    SDL_DisplayMode
+    int isQuit = 0;
+    SDL_Window *win1;
+    SDL_Event event;
+    SDL_Init(SDL_INIT_VIDEO);
 
+    if (SDL_VideoInit(NULL) < 0)
+    {
+
+        printf("error ! \n");
+
+        return a;
+    }
+
+    win1 = SDL_CreateWindow(WINDOW_TITLE, 300 , 300, 800, 600, SDL_WINDOW_RESIZABLE );
+//    SDL_CreateRenderer(win1, -1, flags);
+
+    
+
+    if (win1 != NULL)
+    {
+        SDL_SetWindowTitle(win1, WINDOW_TITLE);
+        printf("window title %s \n",SDL_GetWindowTitle(win1));
+
+
+        SDL_Surface *surface1 =  SDL_GetWindowSurface(win1);
+
+        if (surface1 != NULL)
+        {
+            SDL_UpdateWindowSurface(win1);
+        }
+            
+
+
+        while(0 == isQuit )
+        {
+            if ( SDL_WaitEvent(&event))
+            {
+                if (event.type == SDL_KEYDOWN)
+                {
+                    isQuit = 1;
+                }
+            }
+
+        }
+
+
+        SDL_DestroyWindow(win1);
+        SDL_Quit();
+
+
+    }
+    
     a++;
     return a;
+
 }
+
