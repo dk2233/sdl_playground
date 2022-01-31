c833d2b748a4e41e73267a322a04bd583c8a74fd
danielku <kucharski22daniel@gmail.com>
2022-01-31 22:29:40 +0100

    main updated to be more sensitive for errors

--- source/main.c
+++ source/main.c
@@ -6,41 +6,80 @@
 #include "SDL_surface.h"
 #include "SDL_video.h"
 #include "definitions.h"
+#include "game_types.h"
 #include "sdl_functions.h"
 #include "SDL_image.h"
+#include "SDL_timer.h"
 
 
 int main(int argc, char **argv)
 {
 
     SDL_Event event;
-    SDL_RWops *img1;
-    SDL_Surface *img_surface, *win_surface;
-    SDL_Window *win;
+    SDL_RWops *img1, *img2;
+    SDL_Surface *img1_surface, *img2_surface, *win_surface;
+    MyGame_GfxProperties *GfxItems;
     int a;
+    MyGame_ErrorType gfxStatus = ALL_OK ;
     int isQuit = 0;
 
-    MyGame_GfxInit();
-
-    
-    img1 = SDL_RWFromFile("gfx/img1.png", "rb");
-            
-    if (img1 != NULL)
+    if (MyGame_GfxInit() > 0)
     {
-        img_surface = IMG_LoadPNG_RW(img1);
+       gfxStatus  = ERROR;
+    }
 
 
-        win = MyGame_GetWindow();
+    img1 = SDL_RWFromFile("gfx/img1.png", "rb");
+    img2 = SDL_RWFromFile("gfx/testyuv.bmp", "rb");
 
 
-        win_surface =  SDL_GetWindowSurface(win);
+    if ((img1 != NULL) && (img2 != NULL) && (ALL_OK == gfxStatus ))
+    {
+        img1_surface = IMG_LoadPNG_RW(img1);
+        img2_surface = IMG_LoadBMP_RW(img2);
 
-        img_surface = SDL_ConvertSurface(img_surface, win_surface->format, 0);
 
-        SDL_Rect img1_rect = { 100,100,100,100};
+        SDL_FreeRW(img1);
+        SDL_FreeRW(img2);
 
-        SDL_BlitSurface(img_surface, &img1_rect , win_surface, NULL);
-        SDL_UpdateWindowSurface(win);
+        GfxItems = MyGame_GetGfxItems();
+        win_surface =  SDL_GetWindowSurface(GfxItems->window_struct_p);
+
+        img1_surface = SDL_ConvertSurface(img1_surface, win_surface->format, 0);
+        img2_surface = SDL_ConvertSurface(img2_surface, win_surface->format, 0);
+
+
+        int i = 0;
+        while(i < win_surface->clip_rect.h)
+        {
+            SDL_Rect *img1_rect = &(img1_surface->clip_rect);
+            img1_rect->x += (30 + i);
+            a = SDL_BlitSurface(img1_surface, NULL , win_surface, img1_rect);
+
+
+            SDL_Rect *img2_rect = &(img2_surface->clip_rect);
+
+            img2_rect->x += 100;
+            img2_rect->y += (100 + i);
+
+            SDL_BlitSurface(img2_surface, NULL , win_surface, img2_rect);
+            SDL_UpdateWindowSurface(GfxItems->window_struct_p);
+            SDL_Delay(20);
+
+            //while(0 == isQuit )
+            {
+                if ( SDL_WaitEvent(&event))
+                {
+                    if (event.type == SDL_KEYDOWN)
+                    {
+                        isQuit = 1;
+                        break;
+                    }
+                }
+
+            }
+            
+        }
 
     }
     else
@@ -49,17 +88,6 @@ int main(int argc, char **argv)
     }
 
 
-    while(0 == isQuit )
-    {
-        if ( SDL_WaitEvent(&event))
-        {
-            if (event.type == SDL_KEYDOWN)
-            {
-                isQuit = 1;
-            }
-        }
-
-    }
 
 
 
